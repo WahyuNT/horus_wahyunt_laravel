a9e8cfe2f72015b3b26f59af5ad20f3297ce608b
Wahyu NT <wahyuntx28@gmail.com>
2024-05-16 09:09:36 +0700

    chore: Refactor login method in UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -54,21 +54,21 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        //set validation
+      
         $validator = Validator::make($request->all(), [
             'username'     => 'required',
             'password'  => 'required'
         ]);
 
-        //if validation fails
+  
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        //get credentials from request
+    
         $credentials = $request->only('username', 'password');
 
-        //if auth failed
+    
         if(!$token = auth()->guard('api')->attempt($credentials)) {
             return response()->json([
                 'success' => false,
@@ -76,7 +76,7 @@ class UserController extends Controller
             ], 401);
         }
 
-        //if auth success
+     
         return response()->json([
             'success' => true,
             'user'    => auth()->guard('api')->user(),    
@@ -86,11 +86,11 @@ class UserController extends Controller
 
     public function logout()
     {        
-        //remove token
+      
         $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
 
         if($removeToken) {
-            //return response JSON
+         
             return response()->json([
                 'success' => true,
                 'message' => 'Logout Berhasil!',  
